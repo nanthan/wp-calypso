cafeb682abd725506be132b4c31602028283efbb
Valerie <valerie@automattic.com>
2017-05-12 18:59:58 -0400

    Use arrow functions in email-verification actions

--- client/state/current-user/email-verification/actions.js
+++ client/state/current-user/email-verification/actions.js
@@ -6,10 +6,6 @@ import {
 	EMAIL_VERIFY_STATE_RESET,
 } from 'state/action-types';
 
-export function verifyEmail() {
-	return { type: EMAIL_VERIFY_REQUEST };
-}
+export const verifyEmail = () => ( { type: EMAIL_VERIFY_REQUEST } );
 
-export function resetVerifyEmailState() {
-	return { type: EMAIL_VERIFY_STATE_RESET };
-}
+export const resetVerifyEmailState = () => ( { type: EMAIL_VERIFY_STATE_RESET } );
