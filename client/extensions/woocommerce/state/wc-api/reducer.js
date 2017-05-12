227df69d0c5aeebc41818cb1abb6ee50dddbbee3
Jonathan Belcher <belcherj@gmail.com>
2017-05-12 00:50:46 -0400

    Add currency input with options from API

---
+++ client/extensions/woocommerce/state/wc-api/reducer.js
@@ -0,0 +1,40 @@
+/**
+ * Internal dependencies
+ */
+import error from './error-reducer';
+import currency from './settings/payments/reducer';
+import {
+	SERIALIZE,
+	DESERIALIZE,
+} from 'state/action-types';
+
+const initialState = {};
+
+const handlers = {
+	...currency,
+	...error,
+};
+
+export default function( state = initialState, action ) {
+	const { type, payload } = action;
+	const { siteId } = payload || {};
+	const handler = handlers[ type ];
+
+	// Necessary to ensure this data is not persisted.
+	if ( type === SERIALIZE || type === DESERIALIZE ) {
+		return initialState;
+	}
+
+	if ( handler ) {
+		if ( ! siteId ) {
+			throw new TypeError( `Action ${ type } handled by reducer, but no siteId set on action.` );
+		}
+
+		const siteState = state[ siteId ] || {};
+		const newSiteState = handler( siteState, action );
+
+		return { ...state, [ siteId ]: newSiteState };
+	}
+
+	return state;
+}
